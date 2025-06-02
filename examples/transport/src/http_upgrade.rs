ea9855d2d4222e4e455afbff232dfb503faa3ece
Adesoji Alu <adesoji.alu@gmail.com>
2025-06-02 11:12:06 +0100

    feat: improve HTTP upgrade handling
    Added server startup log with bound address
    Cloned service handler for proper connection handling
    Improved connection logging with client addresses

--- examples/transport/src/http_upgrade.rs
+++ examples/transport/src/http_upgrade.rs
@@ -16,17 +16,22 @@ async fn main() -> anyhow::Result<()> {
     start_server().await?;
     let client = http_client("127.0.0.1:8001").await?;
     let tools = client.list_all_tools().await?;
+    tracing::info!("Available tools: {:#?}", tools);
     client.cancel().await?;
-    tracing::info!("{:#?}", tools);
+    tracing::info!("Client terminated");
     Ok(())
 }
 
 async fn http_server(req: Request<Incoming>) -> Result<hyper::Response<String>, hyper::Error> {
     tokio::spawn(async move {
+        if let Err(e) = async {
         let upgraded = hyper::upgrade::on(req).await?;
         let service = Calculator.serve(TokioIo::new(upgraded)).await?;
         service.waiting().await?;
-        anyhow::Result::<()>::Ok(())
+        Ok::<(), anyhow::Error>(())
+        }.await {
+            tracing::error!("Service error: {:?}", e);
+        }
     });
     let mut response = hyper::Response::new(String::new());
     *response.status_mut() = StatusCode::SWITCHING_PROTOCOLS;
@@ -46,18 +51,19 @@ async fn http_client(uri: &str) -> anyhow::Result<RunningService<RoleClient, ()>
         .insert(UPGRADE, HeaderValue::from_static("mcp"));
     let response = s.send_request(req).await?;
     let upgraded = hyper::upgrade::on(response).await?;
-    let client = ().serve(TokioIo::new(upgraded)).await?;
+    let client = RoleClient.serve(TokioIo::new(upgraded)).await?;
     Ok(client)
 }
 
 async fn start_server() -> anyhow::Result<()> {
     let tcp_listener = tokio::net::TcpListener::bind("127.0.0.1:8001").await?;
+    tracing::info!("Server listening on {}", tcp_listener.local_addr()?);
     let service = hyper::service::service_fn(http_server);
     tokio::spawn(async move {
         while let Ok((stream, addr)) = tcp_listener.accept().await {
-            tracing::info!("accepted connection from: {}", addr);
+            tracing::info!("Accepted connection from: {}", addr);
             let conn = hyper::server::conn::http1::Builder::new()
-                .serve_connection(TokioIo::new(stream), service)
+                .serve_connection(TokioIo::new(stream), service.clone())
                 .with_upgrades();
             tokio::spawn(conn);
         }
