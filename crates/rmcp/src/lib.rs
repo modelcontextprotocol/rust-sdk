1b7b01942264cde116beca7ba2380327f0034cf9
Evian-Zhang <evianzhang1999@163.com>
2026-02-14 09:56:09 +0800

    fix: Remove unnecessary doc-cfg

--- crates/rmcp/src/lib.rs
+++ crates/rmcp/src/lib.rs
@@ -9,25 +9,18 @@ pub use error::{Error, ErrorData, RmcpError};
 /// Basic data types in MCP specification
 pub mod model;
 #[cfg(any(feature = "client", feature = "server"))]
-#[cfg_attr(docsrs, doc(cfg(any(feature = "client", feature = "server"))))]
 pub mod service;
 #[cfg(feature = "client")]
-#[cfg_attr(docsrs, doc(cfg(feature = "client")))]
 pub use handler::client::ClientHandler;
 #[cfg(feature = "server")]
-#[cfg_attr(docsrs, doc(cfg(feature = "server")))]
 pub use handler::server::ServerHandler;
 #[cfg(feature = "server")]
-#[cfg_attr(docsrs, doc(cfg(feature = "server")))]
 pub use handler::server::wrapper::Json;
 #[cfg(any(feature = "client", feature = "server"))]
-#[cfg_attr(docsrs, doc(cfg(any(feature = "client", feature = "server"))))]
 pub use service::{Peer, Service, ServiceError, ServiceExt};
 #[cfg(feature = "client")]
-#[cfg_attr(docsrs, doc(cfg(feature = "client")))]
 pub use service::{RoleClient, serve_client};
 #[cfg(feature = "server")]
-#[cfg_attr(docsrs, doc(cfg(feature = "server")))]
 pub use service::{RoleServer, serve_server};
 
 pub mod handler;
@@ -36,17 +29,12 @@ pub mod transport;
 
 // re-export
 #[cfg(all(feature = "macros", feature = "server"))]
-#[cfg_attr(docsrs, doc(cfg(all(feature = "macros", feature = "server"))))]
 pub use pastey::paste;
 #[cfg(all(feature = "macros", feature = "server"))]
-#[cfg_attr(docsrs, doc(cfg(all(feature = "macros", feature = "server"))))]
 pub use rmcp_macros::*;
 #[cfg(any(feature = "macros", feature = "server"))]
-#[cfg_attr(docsrs, doc(cfg(any(feature = "macros", feature = "server"))))]
 pub use schemars;
 #[cfg(feature = "macros")]
-#[cfg_attr(docsrs, doc(cfg(feature = "macros")))]
 pub use serde;
 #[cfg(feature = "macros")]
-#[cfg_attr(docsrs, doc(cfg(feature = "macros")))]
 pub use serde_json;
