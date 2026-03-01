8d6655b802663477f6d506ccf657d920f5fddff5
Aadam Zocolo <azocolo@gmail.com>
2026-03-01 00:14:16 +0000

    refactor(progress): remove need for spawning on drop
    larger refactor for the way progress is multiplexed
    
    this needed a redesign of the broadcast multiplex logic to a more stateless design.
    
    this design removes the need for mutating any state on drop,
    
    the stream dropping implicitly removes broadcast listeners.
    
    this design also allows for multiple subscribers of the same progress token.

--- crates/rmcp/src/util.rs
+++ crates/rmcp/src/util.rs
@@ -1,5 +1,10 @@
+use futures::Stream;
 use std::pin::Pin;
 
 pub type PinnedFuture<'a, T> = Pin<Box<dyn Future<Output = T> + Send + 'a>>;
 
 pub type PinnedLocalFuture<'a, T> = Pin<Box<dyn Future<Output = T> + 'a>>;
+
+pub type PinnedStream<'a, T> = Pin<Box<dyn Stream<Item = T> + Send + 'a>>;
+
+pub type PinnedLocalStream<'a, T> = Pin<Box<dyn Stream<Item = T> + 'a>>;
